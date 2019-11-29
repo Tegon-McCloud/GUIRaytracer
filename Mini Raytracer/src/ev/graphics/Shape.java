6da90b61f4c429a26faca01871b0d66472e910ed
Emil Lyst <emil@twin.dk>
2019-11-29 09:06:15 +0100

    added a comment

--- Mini Raytracer/src/ev/graphics/Shape.java
+++ Mini Raytracer/src/ev/graphics/Shape.java
@@ -10,6 +10,10 @@ import ev.math.Vec3;
  */
 public interface Shape {
 	
+	/**
+	 * @param r the Ray that we will check if intersects the Shape
+	 * @return the distance from the origin of the Ray to the Shape, or NAN if it doesn't intersect
+	 */
 	public float intersect(Ray r);
 	public Vec2 texCoord(Vec3 surfPos);
 	public Vec3 normal(Vec3 surfPos);
