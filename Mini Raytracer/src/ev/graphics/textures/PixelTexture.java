0f1e12a80b874dbbb216ca0788d7fe34d314b81b
Tegon McCloud <bob.mccheatersen@gmail.com>
2019-12-01 22:20:24 +0100

    add specTex utility to PixelTexture

--- Mini Raytracer/src/ev/graphics/textures/PixelTexture.java
+++ Mini Raytracer/src/ev/graphics/textures/PixelTexture.java
@@ -36,7 +36,7 @@ public class PixelTexture<T> implements Texture2D<T> {
 	}
 	
 	/**
-	 * @param data set the texture.
+	 * @param data the new data.
 	 */
 	public void setData(T[][] data) {
 		this.data = data;
@@ -57,7 +57,11 @@ public class PixelTexture<T> implements Texture2D<T> {
 		for(int i = 0; i < tex.width; i++) {
 			for(int j = 0; j < tex.height; j++) {
 				int rgb = img.getRGB(i, j);
-				texData[i][j] = new Vec3(((rgb >> 16) & 0xff)/255f, ((rgb >> 8) & 0xff)/255f, ((rgb >> 0) & 0xff)/255f);
+				texData[i][j] = new Vec3(
+						(float)((rgb >> 16) & 0xff) / 0xff,	// shift to bit 16 (red color channel) and mask it with 11111111, then normalize
+						(float)((rgb >> 8) & 0xff) / 0xff,	// same for green (bit 8-15)
+						(float)((rgb >> 0) & 0xff) / 0xff	// same for blue (0-7)
+					);
 			}
 		}
 		
@@ -67,14 +71,32 @@ public class PixelTexture<T> implements Texture2D<T> {
 	}
 	
 	/**
-	 * reads an image file f, like it is a specular map where bit 24-31 is the glossiness and 0-23 is the specular intensity
+	 * Reads an image file f as a specular map where bit 24-31 is the glossiness and 0-23 is the specular intensity.
+	 * The intensity is mapped to [0, 1]. The glossiness is first mapped to [0, 1] then multiplied by 12 and used as a power of 2.
+	 * This give glossiness an interval of [1, 4096]
 	 * 
 	 * @param f
 	 * @return a PixelTexture<Vec2> with the specular values from f
 	 * @throws IOException if an IOException occurs reading the file
 	 */
-	public static PixelTexture<Vec2> specTex(File f){
-		return null;
+	public static PixelTexture<Vec2> specTex(File f) throws IOException {
+		BufferedImage img = ImageIO.read(f);
+		PixelTexture<Vec2> tex = new PixelTexture<Vec2>(img.getWidth(), img.getHeight());
+		Vec2[][] texData = new Vec2[tex.width][tex.height];
+		
+		for(int i = 0; i < tex.width; i++) {
+			for(int j = 0; j < tex.height; j++) {
+				int argb = img.getRGB(i, j);
+				texData[i][j] = new Vec2(
+						(float)(argb & 0xffffff) / 0xffffff,							// intensity is bit 0-23
+						(float)Math.pow(2, 12 * ((float)((argb >> 24) & 0xff) / 0xff))	// glossiness is bit 24-31
+					);
+			}
+		}
+		
+		tex.setData(texData);
+		
+		return tex;
 	}
 	
 }
