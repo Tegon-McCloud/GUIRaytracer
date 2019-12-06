ceab58762f26bc2984dd1fb895059c2f8fbe7404
Tegon McCloud <Bob.mccheatersen@gmail.com>
2019-12-06 08:30:24 +0100

    add shape and lightpanel to ccontrolpanel

--- Mini Raytracer/src/ev/gui/ControlPanel.java
+++ Mini Raytracer/src/ev/gui/ControlPanel.java
@@ -8,14 +8,19 @@ import ev.graphics.Camera;
 public class ControlPanel extends JTabbedPane {
 	
 	private CameraPanel cameraPanel;
-	
-	
+	private ShapePanel shapePanel;
+	private LightPanel lightPanel;
 	
 	public ControlPanel() {
 		
 		cameraPanel = new CameraPanel();
 		addTab("Camera", cameraPanel);
 		
+		shapePanel = new ShapePanel();
+		addTab("Shapes", shapePanel);
+		
+		lightPanel = new LightPanel();
+		addTab("Lights", lightPanel);
 		
 	}
 	
---
+++ Mini Raytracer/src/ev/gui/ShapePanel.java
@@ -0,0 +1,8 @@
+package ev.gui;
+
+import javax.swing.JTabbedPane;
+
+@SuppressWarnings("serial")
+public class ShapePanel extends JTabbedPane {
+
+}
